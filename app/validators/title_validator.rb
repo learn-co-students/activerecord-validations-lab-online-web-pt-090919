a3220a6aa4bba259d5b1ff0a882cc3cd30d1e881
Jared S <jaredjames2020@gmail.com>
2020-02-13 19:11:30 +0000

    Done.

---
+++ app/validators/title_validator.rb
@@ -0,0 +1,9 @@
+class TitleValidator < ActiveModel::Validator
+
+  def validate(record)
+    unless record.title && record.title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+      record.errors[:title] << "Nope!"
+    end
+  end
+
+end
