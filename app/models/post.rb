4a6a69fb7912daa32e514fe8677fd537af1c0f31
AnneElizabeth <edmondson.anne@gmail.com>
2020-02-13 21:05:08 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :clickbait?
+
+  def clickbait?
+    if title && !title.match(/Won't Believe|Secret|Top [\d*]|Guess/)
+      errors.add(:clickbait, 'is not enticing enough')
+    end
+  end
 end
