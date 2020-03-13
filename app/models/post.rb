33507c68c463b1e6aad90ab6d96d442824fc4ba9
Eddy Tyannikov <etyannikov@gmail.com>
2020-03-13 22:17:23 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  
+  validates :title, presence: true
+  validate :is_clickbait
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  
+  def is_clickbait
+    if !title.nil?
+      errors.add(:title, "Needs to be clickbait") unless title.include?"Won't Believe"||"Secret"||"Top [number]"||"Guess"
+    else
+    end
+  end
 end
